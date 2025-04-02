6f9993f7d8624736be53d857dc060cf484012a7a
Abdul Hadi Chowdhury <chowdhuryhadiaiub20@gmail.com>
2025-04-03 03:43:37 +0600

    chapter2

--- hello_world.py
+++ hello_world.py
@@ -9,3 +9,30 @@
 # print(name.upper())
 # print(name.lower())
 
+# first_name = "shihab "
+# last_name = "chowdhury"
+# print(first_name.rstrip())
+# name = f"{first_name} {last_name}"  # Properly formatted f-string
+# print(f"Hello, {name.title()}!")
+
+# person_name = "robin"
+# print(f"Hello! {person_name.title()}, would you like to learn some Python today?")
+# print(person_name.lower())
+# print(person_name.upper())
+# print(person_name.title())
+
+# famous_person = "Albert Einstein"
+# quote = '''once said, “A person who never made a
+# mistake never tried anything new.”'''
+
+# print(f"{famous_person} {quote}")
+
+# print(5+3)
+# print(10-2)
+# print(4*2)
+# print(int(16/2))
+
+# favorite_number = 7
+# print(f"my favorite number is {favorite_number}")
+
+import this
