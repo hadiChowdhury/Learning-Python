c7c12a2b1d299a858afc364cfe589e4527fc9a39
Abdul Hadi Chowdhury <chowdhuryhadiaiub20@gmail.com>
2025-04-03 01:16:10 +0600

    Second

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,8 @@
 # message = 'Hello Python Crash Course World 2!!'
 # print(message) #Print another line from the variable
 
-name = 'shihab chowdhury'
-print(name.title())
-print(name.upper())
-print(name.lower())
+# name = 'shihab chowdhury'
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+
